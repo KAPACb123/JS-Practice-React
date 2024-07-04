21ff977f738fe09513668b10ba9e4879c8438fe6
Alexander Bekish <sanyatak3@gmail.com>
2024-07-04 14:34:48 +0300

    feat: add 3 task for JS Practice React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import PhotoContainer from "./components/PhotoContainer/PhotoContainer";
+import Counter from "./components/Counter/Counter";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Photo Gallery</h1>
+        <PhotoContainer/>
+        <Counter/>
+        <Form/>
+      </div>
   );
 }
 
